600842cb9b51604433cbf486b9bc0c4cc6386bfd
samartawil <samartawil01@hotmail.com>
2023-12-11 11:46:15 +0200

    added new pages + css

---
+++ src/components/page2.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/style/Page2.css'
+
+const Page2: React.FC = () => {
+  return (
+    <div className='page-container'>
+      <h1>Page 2</h1>
+    </div>
+  );
+}
+
+export default Page2;
---
+++ src/components/page3.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/style/Page3.css'
+
+const Page3: React.FC = () => {
+  return (
+    <div className='page-container'>
+      <h1>Page 3</h1>
+    </div>
+  );
+}
+
+export default Page3;
