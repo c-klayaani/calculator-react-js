bf94f2792262c85d507ee200f7b50f7031c78684
samartawil <samartawil01@hotmail.com>
2023-12-12 16:30:43 +0200

    added calculator display in page3

--- src/components/Pages/page3.tsx
+++ src/components/Pages/page3.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../../assets/style/Page3.css'
+import { useCalculatorContext } from '../CalculatorContext';
 
 const Page3: React.FC = () => {
+  const {displayContent} = useCalculatorContext();
+
   return (
     <div className='page-container'>
       <h1>Page 3</h1>
+      <p>{displayContent}</p>
     </div>
   );
 }
