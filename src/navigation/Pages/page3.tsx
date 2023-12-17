dcd7dd7cc2c497504b277aa648fd8096a18f372f
samartawil <samartawil01@hotmail.com>
2023-12-17 14:54:25 +0200

    countries api implemented

--- src/navigation/Pages/page3.tsx
+++ src/navigation/Pages/page3.tsx
@@ -1,15 +1,59 @@
-import '../../assets/style/Page3.css'
-import { useCalculatorDisplay } from '../../hooks/useCalculatorDisplay';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../../assets/style/Page3.css';
+
+interface Country {
+isoCode: string;
+name: {
+  language: string;
+  text: string;
+}[];
+officialLanguages: string[];
+}
 
 const Page3: React.FC = () => {
-  const {displayContent} = useCalculatorDisplay();
+const [loading, setLoading] = useState(true);
+const [countries, setCountries] = useState<Country[]>([]);
 
-  return (
-    <div className='page-container'>
-      <h1>Page 3</h1>
-      <p>{displayContent}</p>
-    </div>
-  );
-}
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://openholidaysapi.org/Countries');
+      setCountries(response.data);
+    } finally {
+      setLoading(false);
+    }
+  };
+  fetchData();
+}, []);
+
+return (
+  <div>
+    {loading ? (<h1 className='loading'>Loading...</h1>) : 
+    (
+      <table>
+        <tr>
+          <th>ISO Code</th>
+          <th>Language</th>
+          <th>Country Name</th>
+          <th>Official Languages</th>
+        </tr>
+        {countries.map((country, index) => (
+          <React.Fragment key={index}>
+            {country.name.map((name, i) => (
+              <tr key={`${index}-${i}`}>
+                {i === 0 ? <td rowSpan={country.name.length}>{country.isoCode}</td> : null}
+                <td>{name.language}</td>
+                <td>{name.text}</td>
+                {i === 0 ? <td rowSpan={country.name.length}>{country.officialLanguages.join(', ')}</td> : null}
+              </tr>
+            ))}
+          </React.Fragment>
+        ))}
+      </table>
+    )}
+  </div>
+);
+};
 
 export default Page3;
