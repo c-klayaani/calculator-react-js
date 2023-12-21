8713dfa33479454d851940bb658277fb5ab8e14d
samartawil <samartawil01@hotmail.com>
2023-12-21 17:59:57 +0200

    added sidebar to navigation

--- src/navigation/Pages/components/NavigationBar.tsx
+++ src/navigation/Pages/components/NavigationBar.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../../../assets/style/NavigationBar.css';
+import { Sidebar } from 'primereact/sidebar';
+import { Button } from 'primereact/button';
 
 const NavigationBar: React.FC = () => {
+  const [visible, setVisible] = useState<boolean>(false);
   return (
-    <div className="navigation-bar">
-      <Link to="/page1">Calculator</Link>
-      <Link to="/page2">Page 2</Link>
-      <Link to="/page3">Page 3</Link>
-    </div>
+    
+      <><Sidebar visible={visible} position="top" onHide={() => setVisible(false)}>
+      <div className="navigation-bar">
+        <Link to="/page1">Calculator</Link>
+        <Link to="/page2">Page 2</Link>
+        <Link to="/page3">Page 3</Link>
+      </div>
+    </Sidebar><Button icon="pi pi-arrow-down" onClick={() => setVisible(true)} /></>
+    
   );
 }
 
