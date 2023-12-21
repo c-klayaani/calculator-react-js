9cb913aa9b0e30a5f76146007a4f5e25eb2cef8e
samartawil <samartawil01@hotmail.com>
2023-12-21 15:57:50 +0200

    used prime_react on the table

--- src/navigation/Pages/page3.tsx
+++ src/navigation/Pages/page3.tsx
@@ -3,10 +3,21 @@ import "../../assets/style/Page3.css";
 import { Country } from "../../models/Country";
 import Loader from "../../components/CalculatorComponents/components/Loader";
 import { getCountries } from "../../api/countriesApi";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { FilterMatchMode, FilterOperator } from 'primereact/api';
 
 const Page3: React.FC = () => {
   const [loading, setLoading] = useState(true);
   const [countries, setCountries] = useState<Country[]>([]);
+  const [filters] = useState({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    isoCode: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+    'name.language': { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+    'name.text': { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+    officialLanguage: { value: null, matchMode: FilterMatchMode.STARTS_WITH }
+  });
+
   const fetchData = async () => {
     try {
       const countries = await getCountries();
@@ -24,32 +35,30 @@ const Page3: React.FC = () => {
       {loading ? (
         <Loader />
       ) : (
-        <table>
-          <tr>
-            <th>ISO Code</th>
-            <th>Language</th>
-            <th>Country Name</th>
-            <th>Official Languages</th>
-          </tr>
-          {countries.map((country, index) => (
-            <React.Fragment key={index}>
-              {country.name.map((name, i) => (
-                <tr key={`${index}-${i}`}>
-                  {i === 0 ? (
-                    <td rowSpan={country.name.length}>{country.isoCode}</td>
-                  ) : null}
-                  <td>{name.language}</td>
-                  <td>{name.text}</td>
-                  {i === 0 ? (
-                    <td rowSpan={country.name.length}>
-                      {country.officialLanguages.join(", ")}
-                    </td>
-                  ) : null}
-                </tr>
-              ))}
-            </React.Fragment>
-          ))}
-        </table>
+        <DataTable value={countries} removableSort paginator rows={5} filters={filters} filterDisplay="row" stripedRows tableStyle={{ minWidth: '50rem' }}>
+          <Column field="isoCode" header="ISO Code" filter filterPlaceholder="Search" style={{ minWidth: '12rem', width: '25%' }} sortable />
+          <Column header="Language" filter filterPlaceholder="Search" style={{ minWidth: '12rem', width: '25%' }} sortable 
+            body={(rowData: Country) => (
+              <>
+                {rowData.name.map((name, index) => (
+                  <div key={index}>{name.language}</div>
+                ))}
+              </>
+            )}
+          />
+          <Column header="Country Name" filter filterPlaceholder="Search" style={{ minWidth: '12rem', width: '25%' }} sortable 
+            body={(rowData: Country) => (
+              <>
+                {rowData.name.map((name, index) => (
+                  <div key={index}>{name.text}</div>
+                ))}
+              </>
+            )}
+          />
+          <Column header="Official Languages" filter filterPlaceholder="Search" style={{ minWidth: '12rem', width: '25%' }} sortable 
+            body={(rowData: Country) => rowData.officialLanguages.join(', ')}
+          />
+        </DataTable>
       )}
     </div>
   );
