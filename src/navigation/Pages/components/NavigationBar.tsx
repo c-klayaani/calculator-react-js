9df3eeec713fdbfd0adf280beee562d5d623b687
samartawil <samartawil01@hotmail.com>
2023-12-24 14:14:44 +0200

    edited sidebar styling

--- src/navigation/Pages/components/NavigationBar.tsx
+++ src/navigation/Pages/components/NavigationBar.tsx
@@ -6,17 +6,19 @@ import { Button } from 'primereact/button';
 
 const NavigationBar: React.FC = () => {
   const [visible, setVisible] = useState<boolean>(false);
+
   return (
-    
-      <><Sidebar visible={visible} position="top" onHide={() => setVisible(false)}>
-      <div className="navigation-bar">
-        <Link to="/page1">Calculator</Link>
-        <Link to="/page2">Page 2</Link>
-        <Link to="/page3">Page 3</Link>
-      </div>
-    </Sidebar><Button icon="pi pi-arrow-down" onClick={() => setVisible(true)} /></>
-    
+    <div className="navigation-container">
+      <Sidebar visible={visible} position="top" onHide={() => setVisible(false)} className="custom-sidebar">
+        <div className="navigation-bar">
+          <Link to="/page1">Calculator</Link>
+          <Link to="/page2">Page 2</Link>
+          <Link to="/page3">Page 3</Link>
+        </div>
+      </Sidebar>
+      <Button icon="pi pi-arrow-down" onClick={() => setVisible(true)} />
+    </div>
   );
-}
+};
 
 export default NavigationBar;
