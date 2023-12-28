ca7e487c514e8f7a0f04eaf9ef5bf35a7139ce78
samartawil <samartawil01@hotmail.com>
2023-12-28 15:27:29 +0200

    fixed errors messages

--- src/navigation/Pages/page4.tsx
+++ src/navigation/Pages/page4.tsx
@@ -4,51 +4,59 @@ import { Button } from "primereact/button";
 import { Card } from "primereact/card";
 import { Message } from "primereact/message";
 import { Link } from "react-router-dom";
+import { Tooltip } from "primereact/tooltip";
 import { useValidation } from "../../hooks/useValidation";
 import "../../assets/style/Page4.css";
 
 const Page4: React.FC = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loginError, setLoginError] = useState<string | null>(null);
-  const { isValidEmail } = useValidation();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [emailError, setEmailError] = useState<string | null>(null);
+    const [passwordError, setPasswordError] = useState<string | null>(null);
+    const [loginError, setLoginError] = useState<string | null>(null);
+    const { isValidEmail } = useValidation();
 
-  const handleLogin = () => {
-    if (!isValidEmail(email)) {
-        setLoginError("Invalid email format");
-        return;
-    }
-    else if (email === "sam@hotmail.com" && password === "123@s") setLoginError(null);
-    else setLoginError("Invalid email or password");
-  };
+    const handleLogin = () => {
+        setEmailError(null);
+        setPasswordError(null);
+        setLoginError(null);
+        if (email === "") setEmailError("Email is required");
+        else if (!isValidEmail(email)) setEmailError("Invalid email format");
+        else if (email !== "sam@hotmail.com") setEmailError("Your email is not correct");
+        if (password === "") setPasswordError("Password is required");
+        else if (password !== "123@s") setPasswordError("Your password is not correct");
+        if (!emailError && !passwordError) setLoginError(null);
+      };
 
-  return (
-    <div className="login-form">
-        <Card title="Login" subTitle="Enter your email and password" style={{ width: "300px" }}>
-            <div className="form">
+    return (
+        <div className="login-form">
+            <Card title="Login" subTitle="Enter your email and password" style={{ width: "300px" }}>
+                <div className="form">
                 <div className="input">
                     <label htmlFor="email">Email</label>
-                    <InputText id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <InputText id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} className={emailError ? "p-invalid" : ""}/>
+                    {emailError && <Tooltip target={`#${"email"}`} position="bottom" content={emailError} />}
                 </div>
                 <div className="input">
                     <label htmlFor="password">Password</label>
-                    <InputText id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <InputText id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} className={passwordError ? "p-invalid" : ""}/>
+                    {passwordError && <Tooltip target={`#${"password"}`} position="bottom" content={passwordError} />}
                 </div>
                 <div className="login-signup">
                     <div className="login">
-                            <Button label="Login" onClick={handleLogin} />
+                    <Button label="Login" onClick={handleLogin} />
                     </div>
                     <div className="sign-up">
-                        <Link to="/page5">
-                            <Button label="Sign Up" className="p-button-secondary" />
-                        </Link>
+                    <Link to="/page5">
+                        <Button label="Sign Up" className="p-button-secondary" />
+                    </Link>
                     </div>
                 </div>
                 {loginError && <Message severity="error" text={loginError} />}
-            </div>
-        </Card>
-    </div>
-  );
+                </div>
+            </Card>
+        </div>
+    );
 };
 
 export default Page4;
--- src/navigation/Pages/page5.tsx
+++ src/navigation/Pages/page5.tsx
@@ -2,59 +2,70 @@ import React, { useState } from "react";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
 import { Card } from "primereact/card";
-import { Message } from "primereact/message";
+import { Tooltip } from "primereact/tooltip";
 import { useValidation } from "../../hooks/useValidation";
 import "../../assets/style/Page5.css";
 
+
 const Page5: React.FC = () => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [signupError, setSignupError] = useState<string | null>(null);
-  const { isValidEmail } = useValidation();
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [usernameError, setUsernameError] = useState<string | null>(null);
+    const [emailError, setEmailError] = useState<string | null>(null);
+    const [passwordError, setPasswordError] = useState<string | null>(null);
+    const [confirmPasswordError, setConfirmPasswordError] = useState<string | null>(null);
+    const { isValidEmail } = useValidation();
 
-  const handleSignUp = () => { 
-    if (password ===''  || confirmPassword ==='' || username ==='' || email==='' ) setSignupError("Incomplete form");
-    else if (!isValidEmail(email) && password !== confirmPassword) {
-        setSignupError("Invalid email format and passwords do not match");
-        return;
-    } else if (!isValidEmail(email)) {
-        setSignupError("Invalid email format");
-        return;
-    }
-    else if (password !== confirmPassword) setSignupError("Passwords do not match");
-    else setSignupError(null);
-  };
+    const handleSignUp = () => {
+        setUsernameError(null);
+        setEmailError(null);
+        setPasswordError(null);
+        setConfirmPasswordError(null);
+        if (username === "") setUsernameError("Username is required");
+        if (email === "") setEmailError("Email is required");
+        if (password === "") setPasswordError("Password is required");
+        if (confirmPassword === "") setConfirmPasswordError("Confirm Password is required");
+        if (!isValidEmail(email) && password !== confirmPassword) {
+        setEmailError("Invalid email format");
+        setConfirmPasswordError("Passwords do not match");
+        } else if (!isValidEmail(email)) setEmailError("Invalid email format");
+        else if (password !== confirmPassword) setConfirmPasswordError("Passwords do not match");
+        if (usernameError || emailError || passwordError || confirmPasswordError) return;
+    };
 
-  return (
-      <div className="signup-form">
-          <Card title="Sign Up" subTitle="Create your account" style={{ width: "300px" }}>
-              <div className="form">
-                  <div className="input">
-                      <label htmlFor="usernamr">Username</label>
-                      <InputText id="username" type="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
-                  </div>
-                  <div className="input">
-                      <label htmlFor="email">Email</label>
-                      <InputText id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-                  </div>
-                  <div className="input">
-                      <label htmlFor="password">Password</label>
-                      <InputText id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                  </div>
-                  <div className="input">
-                      <label htmlFor="confirmPassword">Confirm Password</label>
-                      <InputText id="confirmPassword" type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
-                  </div>
-                  <div className="signup">
-                      <Button label="Sign Up" onClick={handleSignUp} />
-                  </div>
-                  {signupError && <Message severity="error" text={signupError} />}
-              </div>
-        </Card>
-      </div>
-  );
+    return (
+        <div className="signup-form">
+            <Card title="Sign Up" subTitle="Create your account" style={{ width: "300px" }}>
+                <div className="form">
+                <div className="input">
+                    <label htmlFor="username">Username</label>
+                    <InputText id="username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} className={usernameError ? "p-invalid" : ""}/>
+                    {usernameError && <Tooltip target={`#${"username"}`} position="bottom" content={usernameError} />}
+                </div>
+                <div className="input">
+                    <label htmlFor="email">Email</label>
+                    <InputText id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} className={emailError ? "p-invalid" : ""}/>
+                    {emailError && <Tooltip target={`#${"email"}`} position="bottom" content={emailError} />}
+                </div>
+                <div className="input">
+                    <label htmlFor="password">Password</label>
+                    <InputText id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} className={passwordError ? "p-invalid" : ""}/>
+                    {passwordError && <Tooltip target={`#${"password"}`} position="bottom" content={passwordError} />}
+                </div>
+                <div className="input">
+                    <label htmlFor="confirmPassword">Confirm Password</label>
+                    <InputText id="confirmPassword" type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} className={confirmPasswordError ? "p-invalid" : ""}/>
+                    {confirmPasswordError && ( <Tooltip target={`#${"confirmPassword"}`} position="bottom" content={confirmPasswordError} />)}
+                </div>
+                <div className="signup">
+                    <Button label="Sign Up" onClick={handleSignUp} />
+                </div>
+                </div>
+            </Card>
+        </div>
+    );
 };
 
 export default Page5;
